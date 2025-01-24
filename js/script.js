07b1690ec0241e62a70d5abd558368ea0348e0cb
KristinaSuvora <137099687+KristinaSuvora@users.noreply.github.com>
2025-01-24 19:02:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 
+document.getElementById("submit").addEventListener("click", () => {
+    const name = document.getElementById("name").value;
+    const dream = document.getElementById("dream").value;
+    const selectedColor = document.querySelector(".color-box.selected")?.dataset.color;
+    localStorage.setItem("userName", name);
+    localStorage.setItem("userDream", dream);
+    localStorage.setItem("userColor", selectedColor);
+    document.body.style.backgroundColor = selectedColor;
+    window.location.href = "home.html";
+});
