bae66ee7513f9da0d598c02a2c87bd30bf971ca9
KristinaSuvora <137099687+KristinaSuvora@users.noreply.github.com>
2025-01-25 22:14:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,28 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.style.backgroundColor = userColor;
     }
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.section');
+
+    sections.forEach(section => {
+        const header = section.querySelector('h2');
+        header.addEventListener('click', () => {
+            const isOpen = section.classList.contains('open');
+
+            // Закрыть все секции
+            sections.forEach(sec => {
+                sec.classList.remove('open');
+                sec.querySelector('.arrow').innerHTML = '&#9660;'; // Стрелка вниз
+                sec.querySelector('.section-content').style.height = '0'; // Устанавливаем высоту 0
+            });
+
+            // Открыть текущую секцию
+            if (!isOpen) {
+                section.classList.add('open');
+                header.querySelector('.arrow').innerHTML = '&#9650;'; // Стрелка вверх
+                const content = section.querySelector('.section-content');
+                content.style.height = `${content.scrollHeight}px`; // Устанавливаем высоту по содержимому
+            }
+        });
+    });
+});
